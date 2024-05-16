626f08569fba96e04a45cc6be939e91cad712e87
Aleksandr Vysotskii <a.vysotckii@bk.ru>
2024-05-16 20:01:35 +0400

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,11 @@
 import { program } from 'commander';
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information');
 
-program.parse();
-if (program.help) program.help();
-// const args = program.opts(); // Получаем аргументы командной строки
-// Проверяем, был ли передан флаг -h или --help, и выводим справку при необходимости
-// if (args.help) {
-//   program.help();
-// }
+program.parse(process.argv);
